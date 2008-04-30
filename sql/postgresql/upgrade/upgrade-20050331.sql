f572a5e7ce0595af18ab862fe8fc8e47343852f1
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2008-04-30 22:30:14 +0000

    - fixed all upgrade scripts to make the re-executable without error   and to include a call to acs_log__debug(key, msg)

--- sql/postgresql/upgrade/upgrade-20050331.sql
+++
@@ -1,4 +0,0 @@
-alter table im_dynfield_layout_pages drop constraint im_dynfield_layout_type_ck;
-
-alter table im_dynfield_layout_pages add constraint im_dynfield_layout_type_ck 
-check (layout_type in ( 'absolute','relative','adp' ));
