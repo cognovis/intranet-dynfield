b3614c2cd26c2688b2500010ae73f63d2d558412
Malte Sussdorff <malte.sussdorff@cognovis.com>
2015-02-12 15:55:17 +0100

    Fixed debug
    Signed-off-by: Malte Sussdorff <malte.sussdorff@cognovis.com>

--- sql/postgresql/upgrade/upgrade-4.1.0.0.0-4.1.0.0.1.sql
+++ sql/postgresql/upgrade/upgrade-4.1.0.0.0-4.1.0.0.1.sql
@@ -1,4 +1,4 @@
-SELECT acs_log__debug('/packages/intranet-dynfield/sql/postgresql/upgrade/upgrade-4.1.0.1.0-4.1.0.0.1.sql','');
+SELECT acs_log__debug('/packages/intranet-dynfield/sql/postgresql/upgrade/upgrade-4.1.0.0.0-4.1.0.0.1.sql','');
 
 
 
